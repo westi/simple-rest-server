87ca8c68af55c550b404d1d01534e6a1b823bd03
Peter Westwood <peter.westwood@ftwr.co.uk>
2015-11-20 13:10:03 +0000

    Update the example server.
    Adds a global method to add a header to all responses with the FQDN of the host, add docs.

--- example-server.py
+++ example-server.py
@@ -1,11 +1,20 @@
-from bottle import Bottle
+import socket
+from bottle import Bottle, request, response
 from rocket import Rocket
 
-my_example_app = Bottle()
+my_example_app = Bottle( )
 
-@my_example_app.route('/hello')
+# A global handler that runs for all requests
+@my_example_app.hook('before_request')
+def before_request():
+	print request.path
+	response.set_header('X-Rocket-Powered', socket.getfqdn())
+
+# Let people test to see if we are alive
+@my_example_app.route('/ping')
 def hello():
-    return "Hello World!"
+    return "pong"
 
+#Let's go
 my_rocket = Rocket( ( '127.0.0.1', 8080, 'self-signed-cert.pem', 'self-signed-cert.pem' ), 'wsgi', { "wsgi_app":my_example_app } )
 my_rocket.start()
