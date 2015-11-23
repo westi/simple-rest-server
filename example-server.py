2cd7beb2d86b1bcffc7ac9bbff23d22edca24dfd
Peter Westwood <peter.westwood@ftwr.co.uk>
2015-11-23 10:30:41 +0000

    Update the server so that it takes the ip/port/ssl cert info on the command line.

--- example-server.py
+++ example-server.py
@@ -1,7 +1,14 @@
 import socket
+import argparse
 from bottle import Bottle, request, response
 from rocket import Rocket
 
+my_argparser = argparse.ArgumentParser()
+my_argparser.add_argument( '--listen-on-ip', '-l', required=True )
+my_argparser.add_argument( '--listen-on-port', '-p', type=int, required=True )
+my_argparser.add_argument( '--ssl-key', required=True )
+my_argparser.add_argument( '--ssl-cert', required=True )
+parsedargs = my_argparser.parse_args()
 my_example_app = Bottle( )
 
 # A global handler that runs for all requests
@@ -16,5 +23,5 @@ def hello():
     return "pong"
 
 #Let's go
-my_rocket = Rocket( ( '127.0.0.1', 8080, 'self-signed-cert.pem', 'self-signed-cert.pem' ), 'wsgi', { "wsgi_app":my_example_app } )
+my_rocket = Rocket( ( parsedargs.listen_on_ip, parsedargs.listen_on_port, parsedargs.ssl_key, parsedargs.ssl_cert ), 'wsgi', { "wsgi_app":my_example_app } )
 my_rocket.start()
